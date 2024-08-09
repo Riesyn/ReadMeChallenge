420e5e5c5bfe03253b28409dd03028fa50b3fae2
Robby <robby@henderson-woods.com>
2024-08-08 23:37:20 -0500

    rough outline for functions on markdown

--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,10 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (license === 'Apache 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === 'GPL 3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL 3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    default:
+      return '';
+    case 'None':
+      return 'No License';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,7 +37,48 @@ function renderLicenseSection(license) { }
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+## Badges
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+
+## Credits
+${data.credits}
+
+${generatelicensesection(data.license)}
+${renderLicenseLink(data.license)}
+
+## License
+${data.license}
+
+## Features
+${data.features}
+
+## How to Contribute
+${data.contribute}
+
+## Tests
+${data.tests}
+
 `;
 }
+
 
 export default generateMarkdown;
