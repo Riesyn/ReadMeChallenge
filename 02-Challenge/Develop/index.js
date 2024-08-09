a4304a42458bbfe630d360f2acd716249d465965
Robby <robby@henderson-woods.com>
2024-08-09 00:26:22 -0500

    got program to init with minimal issues

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,13 +1,89 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application //DONE
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+import Choices from 'inquirer/lib/objects/choices.js';
+
+
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project.',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'none'],
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'List the features of the project.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them.'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'If you have any questions, please feel free to reach out to me at my GitHub profile.'
+    }
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log('README.md created!')
+    );
+
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            writeToFile('README.md', answers);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
 
 // Function call to initialize app
 init();
